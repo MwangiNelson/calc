ddae25ab081dea1e73b04142d9f33bbd071fce43
MwangiNelson <nelsonmwangi197@gmail.com>
2023-02-16 01:36:21 +0300

    updated  calc lol

---
+++ index.js
@@ -0,0 +1,48 @@
+const KEYS = document.getElementsByClassName('key')
+const INPUT_FIELD = document.getElementById('input-field')
+const OPERANDS = document.getElementsByClassName('math_keys')
+const OPERAND_CONTAINER = document.getElementById('operation_container')
+const EQUALS = document.getElementById('equals')
+
+
+
+let btn_array = Array.from(KEYS)
+btn_array.forEach(element => {
+    element.addEventListener("click",()=>{
+        INPUT_FIELD.value += element.innerHTML
+    })
+});
+
+let stored_values = []
+
+let operands_array = Array.from(OPERANDS)
+operands_array.forEach(operand=>{
+    operand.addEventListener('click',()=>{
+        if(stored_values.length == 0){
+            stored_values.push(INPUT_FIELD.value,operand.innerHTML)
+            OPERAND_CONTAINER.innerHTML = `${INPUT_FIELD.value} ${operand.innerHTML}`
+        }else{
+           let new_value = calculate(stored_values)
+           stored_values.push(new_value,operand.innerHTML)
+           OPERAND_CONTAINER.innerHTML = `${new_value} ${operand.innerHTML}`
+        }
+        INPUT_FIELD.value = 00
+
+    })
+})
+
+EQUALS.addEventListener('click',()=>{
+    let ans = calculate(stored_values)
+    INPUT_FIELD.value = ans
+    OPERAND_CONTAINER.innerHTML = ''
+    stored_values.length = 0
+})
+
+function calculate(values){
+    let operator = values[1]
+    let value_start = values[0]
+
+    let ans = eval(value_start+operator+INPUT_FIELD.value)
+    console.log(ans)
+    return ans
+}
